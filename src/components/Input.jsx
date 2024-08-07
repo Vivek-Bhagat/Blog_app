e2b3529b3e035d6555ac224059d01a31d0e80966
Vivek-Bhagat <vivekbhagat1890@hotmail.com>
2024-08-08 01:11:33 +0530

    authLayout, Input form handeling, Post card signup

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,10 @@
 import React, { useId } from "react";
 
-const Input = React.forwardRef(function Input({
-  label,
-  type = "text",
-  className = "",
-  ...props
-}) {
+const Input = React.forwardRef(function Input(
+  { label, type = "text", className = "", ...props },
+  ref
+) {
+  const id = useId();
   return (
     <div className="w-full">
       {label && (
@@ -16,9 +15,9 @@ const Input = React.forwardRef(function Input({
       <input
         type={text}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-              ref={ref}
-              {...props}
-              id={id}
+        ref={ref}
+        {...props}
+        id={id}
       />
     </div>
   );
